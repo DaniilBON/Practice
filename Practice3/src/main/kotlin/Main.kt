a905b1d5d652ae6d8b6903553dd788debbcf203d
DaniilBON <115027998+DaniilBON@users.noreply.github.com>
2022-10-14 22:35:05 +0300

    Add files via upload

---
+++ Practice3/src/main/kotlin/Main.kt
@@ -0,0 +1,32 @@
+import java.util.*
+import.kotlin.math.sqrt
+fun main(args: Array<String>) {
+    // Задание 1
+    val Age: Int = 18
+    val isTeenager: Boolean = Age >= 13 && Age <= 19
+    println(isTeenager)
+    // Задание 2
+    val theirAge: Int = 30
+    val bothTeenager: Boolean = theirAge >= 13 && theirAge <= 19 && isTeenager
+    println(bothTeenager)
+    // Задание 3
+    val reader: String
+
+    // Задание 4
+    val readerBeforeAuthor = reader<author
+    println(readerBeforeAuthor)
+    // Задание 5
+    val Age2: Int = 18
+    if (Age2>=13)
+    // Задание 6
+
+    // Задание 7
+
+    // Задание 8
+
+    // Задание 9
+
+    // Задание 10
+
+
+}
