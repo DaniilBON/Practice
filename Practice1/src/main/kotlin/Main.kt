24f900aafa65262e30c7064a4648bcf91bcb52a2
DaniilBON <115027998+DaniilBON@users.noreply.github.com>
2022-10-14 22:17:43 +0300

    Add files via upload

---
+++ Practice1/src/main/kotlin/Main.kt
@@ -0,0 +1,49 @@
+fun main(args: Array<String>) {
+    // Задание 1
+    val exercises = 13
+    var exercisesSolved = 0
+    exercisesSolved++
+    // Задание 2
+    val ages: Int = 17
+    // Задание 3
+    var aged: Double = 17.0
+    aged = (17.0 + 30) / 2
+    // Задание 4
+    val testNumber = 17
+    val evenOdd = testNumber % 2
+    // Задание 5
+    var answer = 0
+    answer++
+    answer = (answer + 10) * 10
+    answer shr 3
+    // Задание 6
+    var age = 0
+    age = 16
+    print(age)
+    age = 30
+    print(age)
+    // Задание 7
+    val a: Int = 46
+    val b: Int = 10
+    val answer1: Int = (a * 100) + b
+    val answer2: Int = (a * 100) + (b * 100)
+    val answer3: Int = (a * 100) + (b / 10)
+    // Задание 9
+    val aa: Double = 56.2
+    val bb: Double = 32.6
+    val average = (aa + bb) / 2
+    // Задание 9
+    val fahrenheit: Double = 129.2
+    val celcius = (129.2 - 32) / 1.8
+    println(exercisesSolved)
+    println(aged)
+    println(evenOdd)
+    println(answer)
+    println(answer1)
+    println(answer2)
+    println(answer3)
+    println(((5 * 3) - (4 / 2) * 2)) // Задание 8
+    println(average)
+    println(celcius)
+
+}
